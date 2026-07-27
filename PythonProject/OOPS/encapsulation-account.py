92045f207526ca88cd552b30b9abfd1205ab2bb7
amitsingh2102 <singhamit8146@gmail.com>
2026-07-27 17:10:47 +0530

    done

---
+++ PythonProject/OOPS/encapsulation-account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+
+    def __init__(self):
+        self.__balance = 10000
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount()
+account.deposit(5000)
+account.withdraw(2000)
+
+print(account.get_balance())
